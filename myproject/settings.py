c54622195d482e6be8cf69892b893ed279becc71
spectkiller <rajhrishabh892@gmail.com>
2023-08-26 06:37:07 +0530

    Fixed Feedback services

--- myproject/settings.py
+++ myproject/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "rajhrishabh892@gmail.com"
-EMAIL_HOST_PASSWORD = "vzgzkexqtshzsriu"
+EMAIL_HOST_PASSWORD = "cvsvckpxnebpxnau"
