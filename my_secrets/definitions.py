cd92028acac687f8a83e0712fec930eae6e16ee2
spectkiller <rajhrishabh892@gmail.com>
2023-08-28 07:34:48 +0530

    Fixing bugs here and there

---
+++ my_secrets/definitions.py
@@ -0,0 +1,12 @@
+#Email Secrets key
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "rajhrishabh892@gmail.com"
+EMAIL_HOST_PASSWORD = "cvsvckpxnebpxnau"
+
+#Recaptcha secret key
+RECAPTCHA_PUBLIC_KEY = '6LdLet0nAAAAAH4rbuztELRicoe-IcasYSR7AWgu'
+RECAPTCHA_PRIVATE_KEY = '6LdLet0nAAAAAKHlHgTbta9V8ekEZQucGw_1e3TO'
